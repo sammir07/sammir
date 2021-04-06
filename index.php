39be8da6a21622b7b3a74f4bc55ca38a2cade621
sammir07 <70575651+sammir07@users.noreply.github.com>
2021-04-06 13:09:23 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,259 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Al-muzammil</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/typed.js/2.0.11/typed.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"/>
+
+</head>
+<body>
+    <div class="scroll-up-btn">
+        <i class="fas fa-angle-up"></i>
+    </div>
+    <nav class="navbar">
+        <div class="max-width">
+            <div class="logo"><a href="#">SAMMIR<span> el-betis.</span></a></div>
+            <ul class="menu">
+                <li><a href="#home" class="menu-btn">Home</a></li>
+                <li><a href="#about" class="menu-btn">About</a></li>
+                <li><a href="#services" class="menu-btn">Services</a></li>
+                <li><a href="#skills" class="menu-btn">Skills</a></li>
+                <li><a href="#teams" class="menu-btn">Teams</a></li>
+                <li><a href="#contact" class="menu-btn">Contact</a></li>
+            </ul>
+            <div class="menu-btn">
+                <i class="fas fa-bars"></i>
+            </div>
+        </div>
+    </nav>
+
+    <!-- home section start -->
+    <section class="home" id="home">
+      <div class="max-width">
+          <div class="row">
+            <div class="home-content">
+                <div class="text-1">Assalamulaikum, Nama Saya</div>
+                <div class="text-2">Al-muzammil</div>
+                <div class="text-3">Dan saya adalah seorang <span class="typing"></span></div>
+                <a href="#">Ayok lah</a>
+            </div>
+          </div>
+      </div>
+    </section>
+
+    <!-- about section start -->
+    <section class="about" id="about">
+        <div class="max-width">
+            <h2 class="title">About me</h2>
+            <div class="about-content">
+                <div class="column left">
+                    <img src="images/back2.jpg" alt="">
+                </div>
+                <div class="column right">
+                    <div class="text">Saya Al-muzammil dan saya <span class="typing-2"></span></div>
+                    <p>Perkenalkan, nama saya Al-muzammil Bin Husnan, biasanya saya dipanggil SAMMIR atau DEK MIL, saya lahir di Aceh, saya tinggal di aceh untuk sementara, karna ada project yang harus diselesaikan, dan disini saya dan team siap membantu anda dalam melakukan pekerjaan, baik itu Project besar atau kecil, saya akan kembali ke jerman, perancis, turki, Amerika dan Dubai pada tahun 2030 untuk melanjutkan pekerjaan saya disana. dan saya akan pulang kembali ke aceh pada tahun 2045. semoga panjang umur, sehat, dan rezeki melimpah untuk kita semua, Aamiin.</p>
+                    <a href="#">Download CV</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- services section start -->
+    <section class="services" id="services">
+        <div class="max-width">
+            <h2 class="title">My services</h2>
+            <div class="serv-content">
+                <div class="card">
+                    <div class="box">
+                        <i class="fas fa-paint-brush"></i>
+                        <div class="text">Web Design</div>
+                        <p>Siap membantu dalam pembuatan web design, dan design apa saja dan melayani anda sepenuh nya.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <i class="fas fa-chart-line"></i>
+                        <div class="text">Advertising</div>
+                        <p>Siap membantu anda dalam membuat iklan, baik berupa iklan tv atau umum dan melayani anda sepenuh nya.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <i class="fas fa-code"></i>
+                        <div class="text">Apps Design</div>
+                        <p>Siap membantu dalam membuat aplikasi, dan design dan melayani anda sepenuh nya.</p>
+                    </div>
+                </div>
+               </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- skills section start -->
+    <section class="skills" id="skills">
+        <div class="max-width">
+            <h2 class="title">My skills</h2>
+            <div class="skills-content">
+                <div class="column left">
+                    <div class="text">My team creative skills & experiences.</div>
+                    <p>Ini adalah skill dari tim saya, dan disini merupakan ahli dari segala ahli teknologi, dan kami siap membantu anda dalam menyelesaikan sesuatu masalah yang anda hadapi, baik itu pekerjaan maupun pribadi, terima kasih untuk semua.</p>
+                    <a href="#">Read more</a>
+                </div>
+                <div class="column right">
+                    <div class="bars">
+                        <div class="info">
+                            <span>information technology</span>
+                            <span>90%</span>
+                        </div>
+                        <div class="line html"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>Nuclear Engineering</span>
+                            <span>60%</span>
+                        </div>
+                        <div class="line css"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>Desainer</span>
+                            <span>80%</span>
+                        </div>
+                        <div class="line js"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>mechanical Engineering</span>
+                            <span>50%</span>
+                        </div>
+                        <div class="line php"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>majoring in hackers</span>
+                            <span>70%</span>
+                        </div>
+                        <div class="line mysql"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- teams section start -->
+    <section class="teams" id="teams">
+        <div class="max-width">
+            <h2 class="title">My teams</h2>
+            <div class="carousel owl-carousel">
+                <div class="card">
+                    <div class="box">
+                        <img src="images/t1.jpg" alt="">
+                        <div class="text">Baby Boss</div>
+                        <p>Dia adalah seorang Boss Bayi yang sangat cerdas dalam menyelesaikan suatu masalah, makanya disini saya memakai dia untuk Team.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <img src="images/t2.jpg" alt="">
+                        <div class="text">Spongebob</div>
+                        <p>Dia merupakan orang yang dilaut dalam yang bisa mematai semua orang untuk dalam sekejap, saya suka dengan spongebob karna dia bisa membuat orang selalu semangat.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <img src="images/t3.jpg" alt="">
+                        <div class="text">Patrick</div>
+                        <p>Bintang laut yang tidak mempunyai otak, tapi bisa berpikir seperti orang yang punya otak, saya salut sama patrick, makanya dia team saya.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <img src="images/t4.jpg" alt="">
+                        <div class="text">Upin</div>
+                        <p>Sikembar yang cerdas dan pandai dalam bertindak dan Aksi yang yang sangat luar biasa dalam melakukan sebuah pekerjaan, My team is the best.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <img src="images/t5.jpg" alt="">
+                        <div class="text">Ipin</div>
+                        <p>Sikembar yang cerdas dan pandai dalam bertindak dan Aksi yang yang sangat luar biasa dalam melakukan sebuah pekerjaan, My team is the best.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- contact section start -->
+    <section class="contact" id="contact">
+        <div class="max-width">
+            <h2 class="title">Contact me</h2>
+            <div class="contact-content">
+                <div class="column left">
+                    <div class="text">Get in Touch</div>
+                    <p>Selamat dan semoga sukses untuk kita semua, silahkan bertanya apa yang tidak anda pahami dan silahkan tegur kami kalau kami salah, "orang sopan akan selalu dihargai" mental harus kuat, jangan lebay ataupun alay jadi laki.</p>
+                    <div class="icons">
+                        <div class="row">
+                            <i class="fas fa-user"></i>
+                            <div class="info">
+                                <div class="head">Nama</div>
+                                <div class="sub-title">Al-muzammil</div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <i class="fas fa-map-marker-alt"></i>
+                            <div class="info">
+                                <div class="head">Alamat</div>
+                                <div class="sub-title">Banda Aceh, Indonesia</div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <i class="fas fa-envelope"></i>
+                            <div class="info">
+                                <div class="head">Email</div>
+                                <div class="sub-title">almuzammil05@gmail.com</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="column right">
+                    <div class="text">Message me</div>
+                    <form class="contact-form" action="#" method="POST">
+                        <div class="fields">
+                            <div class="field name">
+                                <input type="text" class="fullname" placeholder="Name">
+                            </div>
+                            <div class="field email">
+                                <input type="text" class="email-input" placeholder="Email">
+                            </div>
+                        </div>
+                        <div class="field">
+                            <input type="text" class="subject" placeholder="Subject">
+                        </div>
+                        <div class="field textarea">
+                            <textarea class="message" cols="30" rows="10" placeholder="Message.."></textarea>
+                        </div>
+                        <div class="button-area">
+                            <button class="send-msg" type="submit" name="send">Send message</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- footer section start -->
+    <footer>
+        <span>Created By <a href="https://muzammil93.wordpress.com/">Sammir El-betis</a> | <span class="far fa-copyright"></span> 2020 All rights reserved.</span>
+    </footer>
+
+    <script src="script.js"></script>
+</body>
+</html>
